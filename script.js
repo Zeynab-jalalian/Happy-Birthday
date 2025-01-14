bad316061c864cf75fab7be94bae1c656fe5e903
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-14 16:06:08 +0330

    hb

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ button click
 if todaydate=birthdate
 */
 let button=document.getElementById('checkButton'); //دسترسی به دکمه
+let message=document.getElementById('message');//دسترسی به پیغام نهایی
 button.addEventListener('click',function(){
     let input=document.getElementById('Birthdate').value; //دسترسی به تاریخ تولد
-    let todayDate=new Date();
-    let todayMonth=todayDate.getMonth();
-    let todayDay=todayDate.getDate();
-    if(input==){
+    let todayDate=new Date(); // تاریخ امروز
+    let todayMonth=todayDate.getMonth(); //ماه امروز
+    let todayDay=todayDate.getDate(); //روز امروز
 
+    let birthDate=new Date(); // تاریخ  تولد
+    let birthMonth=birthDate.getMonth(); //ماه تولد
+    let birthDay=birthDate.getDate(); //روز تولد
+
+    if(birthMonth==todayMonth && birthDay==todayDay){
+        message.innerHTML="Happy Birthday!"
     }else{
-        
+        message.innerHTML="It's not your birthday!"
     }
 })
