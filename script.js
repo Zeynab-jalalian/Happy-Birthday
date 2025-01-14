a1726f5f9b17d0faa6fbfd876aced3e12342a55b
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-14 16:00:02 +0330

    js step1

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+button click 
+if todaydate=birthdate
+*/
+let button=document.getElementById('checkButton'); //دسترسی به دکمه
+button.addEventListener('click',function(){
+    let input=document.getElementById('Birthdate').value; //دسترسی به تاریخ تولد
+    let todayDate=new Date();
+    let todayMonth=todayDate.getMonth();
+    let todayDay=todayDate.getDate();
+    if(input==){
+
+    }else{
+        
+    }
+})
